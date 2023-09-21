53959cba339490853a816cb6df48f7f033dbec22
Iroshana Ravishan <78033943+iroshanaRavishan@users.noreply.github.com>
2023-09-21 22:21:21 +0530

    Update streamlit_app.py
    added a new line to show a new values in the table

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_rows)
 
 
-
-
 add_my_fruit = streamlit.text_input('What fruit would you like add?','jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit'); ")
